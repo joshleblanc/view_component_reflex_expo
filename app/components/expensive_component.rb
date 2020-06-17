7bf071712607df840fa71f0acde1703e7f7e64d8
Joshua LeBlanc <joshleblanc94@gmail.com>
2020-06-17 19:56:00 -0300

    feat: add timer to expensive component

--- app/components/expensive_component.rb
+++ app/components/expensive_component.rb
@@ -1,12 +1,18 @@
 class ExpensiveComponent < ViewComponentReflex::Component
   def initialize
     @loading = false
+    @counter = 3
   end
 
   def execute
+    @counter = 3
     @loading = true
     refresh!
-    sleep 3
+    @counter.times do
+      sleep 1
+      @counter -= 1
+      refresh!
+    end
     @loading = false
   end
 end
