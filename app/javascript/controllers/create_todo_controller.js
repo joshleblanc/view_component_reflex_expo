528641055c542e8fb73fc4f6064ebd87d0c2dfb4
Joshua LeBlanc <joshleblanc94@gmail.com>
2020-06-20 09:06:31 -0300

    fix: clear todo input on create

---
+++ app/javascript/controllers/create_todo_controller.js
@@ -0,0 +1,9 @@
+import ApplicationController from './application_controller'
+
+export default class extends ApplicationController {
+  add(e) {
+    e.preventDefault();
+    Array.from(e.target.elements).forEach(e => e.value = "");
+    this.stimulate("CreateTodoComponent#add", e.target);
+  }
+}
