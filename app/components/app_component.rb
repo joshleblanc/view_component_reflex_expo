f39377190c90aa9696de2b3e6aa0e5cab0f0fad9
Joshua LeBlanc <joshleblanc94@gmail.com>
2020-07-03 08:04:54 -0300

    refactor: simplify menu

--- app/components/app_component.rb
+++ app/components/app_component.rb
@@ -1,26 +1,13 @@
+# frozen_string_literal: true
+
 class AppComponent < ViewComponent::Base
   def components
-    {
-      "index" => {
-        label: "Index",
-        to: "/"
-      },
-      "local_state" => {
-        label: "Local State",
-        to: "/local_state"
-      },
-      "loaders" => {
-        label: "Loaders",
-        to: "/loaders"
-      },
-      "todo" => {
-        label: "Todos",
-        to: "/todos"
-      },
-      "table_expo" => {
-        label: "Data table",
-        to: "/data_table"
-      }
-    }
+    [
+      { label: 'Index', to: '/' },
+      { label: 'Local State', to: '/local_state' },
+      { label: 'Loaders', to: '/loaders' },
+      { label: 'Todos', to: '/todos' },
+      { label: 'Data table', to: '/data_table' }
+    ]
   end
 end
