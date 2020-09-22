d158d56227a3ff889df5d5a96774c630ea4d3cb7
Joshua LeBlanc <jleblanc@omics.com>
2020-09-22 17:40:50 -0300

    chore: revert config changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,17 +3,17 @@ Rails.application.configure do
   config.session_store :cache_store
 
   # Code is not reloaded between requests.
-  config.cache_classes = false
+  config.cache_classes = true
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
   # Rake tasks automatically ignore this option for performance.
-  config.eager_load = false
+  config.eager_load = true
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
-  config.action_controller.perform_caching = false
+  config.action_controller.perform_caching = true
 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
