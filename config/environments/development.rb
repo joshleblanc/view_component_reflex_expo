f5507ee71f788eb2c8980011997001412cb38c9b
Joshua LeBlanc <jleblanc@hey.com>
2021-01-14 16:57:30 +0000

    add code-server host

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,7 +2,8 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   config.session_store :cache_store
-
+  config.hosts << "3000.code.monolith.sh"
+  
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
