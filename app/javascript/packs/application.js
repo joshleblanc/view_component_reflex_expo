4e6b2d040e1502d8a3084621eb106fc7863b176c
Joshua LeBlanc <jleblanc@hey.com>
2021-03-11 14:52:18 +0000

    chore: update deps

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,9 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+
+var Turbolinks = require("turbolinks")
+Turbolinks.start()
 require("channels")
 
 
